37cef8151385f45d71d633fb4bdc96e658716bb2
RUDY MARTINS <martinsosr@gmail.com>
2023-10-12 12:39:38 +0200

    test basics 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import PokemonCard from "./components/pokemonCard"; 
 
-function MyPokemonCard() {
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+/*const pokemon = pokemonList[0];*/
+
+function App() {
+ 
   return (
     <div>
-      <PokemonCard/>
-     </div>
+      <PokemonCard pokemon={pokemonList[0]}/>
+    </div>
   );
 }
 
-export default MyPokemonCard;
+export default App;
 
 
 
