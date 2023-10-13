e8512fb4862aea60662d6b26476c4f759803d123
RUDY MARTINS <martinsosr@gmail.com>
2023-10-13 14:33:58 +0200

    ajout button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import PokemonCard from "./components/pokemonCard"; 
+import { useState } from "react";
 
 const pokemonList = [
   {
@@ -7,18 +8,52 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    name: "mew",
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
   },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+  }
+
 ];
 
 /*const pokemon = pokemonList[0];*/
 
 function App() {
- 
+
+const [pokemonIndex, setpokemonIndex] = useState(0);
+
+const pkmSuivant = () => {setpokemonIndex(pokemonIndex+ 1)}
+const pkmPrecedent = () => {setpokemonIndex (pokemonIndex-1 )}
+
   return (
+    
     <div>
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      <button 
+        disabled={pokemonIndex === 0}
+        onClick= {pkmPrecedent}>"précédent"</button> 
+      <button 
+        disabled={pokemonIndex >= pokemonList.length -1}
+        onClick= {pkmSuivant}>"suivant"</button>
+      
+      <p>index :{pokemonIndex}</p>
     </div>
+    
+  
   );
 }
 
